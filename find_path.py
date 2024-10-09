deceaae13064dae37146fdea6036d3519b3976a1
prinklee <priyaadiga6@gmail.com>
2024-10-09 15:04:02 -0400

    revision #1

--- find_path.py
+++ find_path.py
@@ -1,7 +1,6 @@
 import numpy as np 
 import sys
 
-
 #path output should be a list of the coordinates  
 class node:
     def __init__(self, x, y, dist=-1, ancestor=None):
@@ -16,13 +15,13 @@ class node:
 def get_neighbors(n,map,checked_vertices):
     neighbors = [(n.x-1,n.y), (n.x+1,n.y), (n.x,n.y-1), (n.x,n.y+1)]
 
-    if n.x-1==-1 or map[n.x-1][n.y]== '*':
+    if n.x-1==-1 or map[n.x-1][n.y]== -1:
         neighbors.remove((n.x-1,n.y))
-    if n.x+1==len(map) or map[n.x+1][n.y] == '*':
+    if n.x+1==len(map) or map[n.x+1][n.y] == -1:
         neighbors.remove((n.x+1,n.y))
-    if n.y-1==-1 or map[n.x][n.y-1] == '*':
+    if n.y-1==-1 or map[n.x][n.y-1] == -1:
         neighbors.remove((n.x,n.y-1))
-    if n.y+1==len(map[0]) or map[n.x][n.y+1] == '*':
+    if n.y+1==len(map[0]) or map[n.x][n.y+1] == -1:
         neighbors.remove((n.x,n.y+1))
 
     for item in neighbors.copy():
@@ -33,62 +32,55 @@ def get_neighbors(n,map,checked_vertices):
 def find_path(map, all_vertices, s,e):
     checked_vertices = set() 
     current_vertices = {s}
-
-    while len(checked_vertices.copy()) != len(all_vertices.keys()):
+    check = True
+    while check:
         for vertex in current_vertices.copy():
             i, j = vertex[0], vertex[1]
             neighbors = get_neighbors(all_vertices[(i,j)],map,checked_vertices)
             current_vertices.update(neighbors)
-
             for neighbor in neighbors.copy():
                 a, b = neighbor[0], neighbor[1]
-                if map[a][b] == '-':
-                    map[a][b] = int(map[i][j])+1
+                if map[a][b] == 0:
+                    map[a][b] = map[i][j]+1
                     all_vertices.update({(a,b): node(a,b,map[a][b], (i,j))})
-                elif int(map[a][b]) > int(map[i][j]) + 1:
-                    map[a][b] = int(map[i][j]) + 1
-                    all_vertices.update({(a,b): node(a,b,map[a][b], (i,j))})
+                    if a == e[0] and b == e[1]:
+                        check = False
             current_vertices.remove(vertex)
             checked_vertices.add(vertex)
         
-
     path = list()
-
     path.append(e)
-
     while(all_vertices[path[-1]].anc != None):
         path.append(all_vertices[path[-1]].anc)
 
     path.reverse()
 
-    return path
+    return path, map
 
 def load_map(filename):
-    map = np.genfromtxt(filename,delimiter=" ", dtype='str')
-    
+    l = np.genfromtxt(filename,delimiter=" ", dtype='str')
+    map = np.zeros((len(l),len(l[0])), dtype='int')
+
     all_vertices = dict()
 
     for x in range(len(map)):
         for i in range(len(map[0])):
-            if map[x][i] == '0':
-                map[x][i] = '-'
-                all_vertices.update({(x,i): node(x,i)})
-            if map[x][i] == 'S':
-                map[x][i] = '0'
+            if l[x][i] == 'S':
+                map[x][i] = 0
                 start = (x,i)
                 all_vertices.update({(x,i): node(x,i,0)})
-            if map[x][i] == '1':
-                map[x][i] = '*'
-            if map[x][i] == 'X':
+            elif l[x][i] == 'X':
                 end = (x,i)
-                map[x][i] = '-'
+                map[x][i] = 0
+            elif l[x][i] == '1':
+                map[x][i] = -1
                 all_vertices.update({(x,i): node(x,i,-1)})
     return map, all_vertices, start, end
 
-
 def main():
     map, all_vertices, start, end = load_map(sys.argv[1])
-    print(f'The quickest path is {find_path(map,all_vertices, start,end)}.')
+    path, map = find_path(map,all_vertices, start,end)
+    print(f'The quickest path is {path}.')
 
 if __name__ == '__main__':
     main()
